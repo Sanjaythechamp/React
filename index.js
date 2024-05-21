9df14df613c3dd15eccf32b7aab1aabf94c6b77f
sanjay bisht <129532013+Sanjaythechamp@users.noreply.github.com>
2024-05-21 15:27:23 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+/*
+first code
+import './index.css'
+
+import './index.css'
+// import ReactDom from 'react-dom'
+import App from './App.js'
+
+ReactDom.render(<App/>,document.getElementById('root'));
+*/
+
+// Another code
+
+
+// this code is of content,header,footer.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
+
+
+
+
+// third code start 
